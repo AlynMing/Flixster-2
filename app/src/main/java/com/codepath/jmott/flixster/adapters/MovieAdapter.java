109053d252ed3a8f53a514db5cd15f732557a824
Jamie Mott <jamie.c.mott@gmail.com>
2020-09-29 15:09:47 -0600

    Added shared transitions for title and overview

--- app/src/main/java/com/codepath/jmott/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/jmott/flixster/adapters/MovieAdapter.java
@@ -1,5 +1,6 @@
 package com.codepath.jmott.flixster.adapters;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -11,11 +12,14 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.codepath.jmott.flixster.DetailActivity;
+import com.codepath.jmott.flixster.MainActivity;
 import com.codepath.jmott.flixster.R;
 import com.codepath.jmott.flixster.models.Movie;
 
@@ -85,7 +89,11 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
                 public void onClick(View v) {
                     Intent i = new Intent(context, DetailActivity.class);
                     i.putExtra("movie", Parcels.wrap(movie));
-                    context.startActivity(i);
+                    Pair<View, String> p1 = Pair.create((View)tvTitle, "movieTitle");
+                    Pair<View, String> p2 = Pair.create((View)tvOverview, "movieOverview");
+                    ActivityOptionsCompat options = ActivityOptionsCompat.
+                            makeSceneTransitionAnimation((Activity) context, p1, p2);
+                    context.startActivity(i,options.toBundle());
                 }
             });
         }
